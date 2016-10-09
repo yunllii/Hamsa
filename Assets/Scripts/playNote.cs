abe67060b6a5b6bfdf58ad2abc187e021e1ccd3b
Yun Li <yl3795@nyu.edu>
2016-10-08 23:38:38 -0400

    DemoScenes

---
+++ Assets/Scripts/playNote.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playNote : MonoBehaviour {
+
+
+    AudioSource do_note;
+	// Use this for initialization
+	void Start () {
+        do_note = GetComponent<AudioSource>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        
+            do_note.Play();
+            Debug.Log("Haha");
+
+
+    }
+}
